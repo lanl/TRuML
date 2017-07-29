8cdc45f70d5c637b284f51643a216a2d50c90a76
Ryan T. Suderman <ryants@lanl.gov>
2017-07-29 08:02:08 -0600

    main function for conversion

---
+++ rbconvert/convert.py
@@ -0,0 +1,34 @@
+import argparse as ap
+import rbexceptions
+import readers
+import re
+
+
+# List of arguments to be implemented:
+# add flag for outputting Kappa file with/without x{y} rate notation
+def main():
+    parser = ap.ArgumentParser()
+    parser.add_argument('-b', '--bngl_files', nargs='+', metavar='model.bngl', help='BNGL files to convert to Kappa')
+    parser.add_argument('-k', '--kappa_files', nargs='+', metavar='model.ka', help='Kappa files to convert to BNGL')
+    parser.add_argument('-nf', '--no_print_funcs', action='store_false',
+                        help='writes dynamic quantities as variables instead of observables (Kappa)')
+    args = parser.parse_args()
+
+    print args
+
+    if args.bngl_files is None and args.kappa_files is None:
+        raise rbexceptions.NoModelsException("\nNo models specified.  See `convert.py -h` for information\n")
+    elif args.bngl_files is None:
+        for kf in args.kappa_files:
+            kr = readers.KappaReader(kf)
+            model = kr.parse()
+            model.write_as_bngl(re.sub('ka', 'bngl', kf))
+    else:
+        for bf in args.bngl_files:
+            br = readers.BNGLReader(bf)
+            model = br.parse()
+            model.write_as_kappa(re.sub('bngl', 'ka', bf), args.print_funcs)
+
+
+if __name__ == "__main__":
+    main()
