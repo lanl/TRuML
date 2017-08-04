95bbf687057bcd508735869eab224c5629816d6e
Ryan T. Suderman <ryants@lanl.gov>
2017-08-04 15:58:51 -0600

    added flag to print BNGL rules using both operators

--- rbconvert/convert.py
+++ rbconvert/convert.py
@@ -13,6 +13,8 @@ def main():
     parser.add_argument('-k', '--kappa_files', nargs='+', metavar='model.ka', help='Kappa files to convert to BNGL')
     parser.add_argument('-nf', '--no_print_funcs', action='store_false',
                         help='writes dynamic quantities as variables instead of observables (Kappa)')
+    parser.add_argument('-dp', '--dot_and_plus', action='store_true',
+                        help='outputs BNGL files with both intra- and intermolecular rules')
     parser.add_argument('-v', '--verbose', action='store_true', help='verbose output')
     parser.add_argument('-d', '--debug', action='store_true', help='verbose output useful for debugging')
     parser.add_argument('-l', '--log_file', metavar='convert.log', help='Specify log file')
@@ -38,7 +40,7 @@ def main():
         for kf in args.kappa_files:
             kr = readers.KappaReader(kf)
             model = kr.parse()
-            model.write_as_bngl(re.sub('ka', 'bngl', kf))
+            model.write_as_bngl(re.sub('ka', 'bngl', kf), args.dot_and_plus)
     else:
         for bf in args.bngl_files:
             br = readers.BNGLReader(bf)
