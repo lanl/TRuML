d3ea46911dafdbbfabf41f79af39c82e91e5decc
Ryan T. Suderman <ryants@lanl.gov>
2017-08-08 21:26:30 -0600

    runner script for executing TRuML from source tree

---
+++ truml-runner.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+
+
+"""truml.truml-runner: Convenience script for running TRuML from the source tree"""
+
+
+from truml.truml import main
+
+if __name__ == '__main__':
+    main()
